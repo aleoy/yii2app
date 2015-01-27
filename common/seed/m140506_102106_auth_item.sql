0b7543b184b63433d6fbbe85b02af828d4187d7e
Vitor Silva <vitor@aleoy.com>
2015-01-27 23:22:48 +0200

    seed data folder

---
+++ common/seed/m140506_102106_auth_item.sql
@@ -0,0 +1,16 @@
+INSERT INTO auth_item (name, type,
+  description,
+  rule_name,
+  data,
+  created_at,
+  updated_at
+) 
+VALUES(
+  'admin',
+  '1',
+  NULL,
+  NULL,
+  '',
+  '1421956275',
+  '1421956275'
+);
---
+++ common/seed/m140506_102110_auth_assignment.sql
@@ -0,0 +1,11 @@
+INSERT INTO auth_assignment (
+item_name,
+user_id,
+created_at
+)
+VALUES
+(
+'admin',
+'1',
+'1421956275'
+);
