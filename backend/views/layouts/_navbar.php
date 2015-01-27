3913d1d23585a25ffaf6b02507627b37dc5f8a7e
Vitor Silva <vitor@aleoy.com>
2015-01-27 23:28:12 +0200

    navbar refactor

--- backend/views/layouts/_navbar.php
+++ backend/views/layouts/_navbar.php
@@ -14,29 +14,20 @@ use yii\helpers\Url;
             if (Yii::$app->user->isGuest) {
                 $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
             } else {
-                // $menuItems[] = [
-                //     'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                //     'url' => ['/site/logout'],
-                //     'linkOptions' => ['data-method' => 'post']
-                // ];
                 $menuItems = [
                     ['label' => 'Home', 'url' => ['/site/index']],
                     ['label' => 'Location',
                      'items' => [
                         '<li class="dropdown-header">Country</li>',
-                        ['label' => 'Create', 'url' => Url::to(['/location/country/create'])],
                         ['label' => 'Search', 'url' => Url::to(['/location/country/index'])],
                         '<li class="divider"></li>',
                         '<li class="dropdown-header">City</li>',
-                        ['label' => 'Create', 'url' => Url::to(['/location/city/create'])],
                         ['label' => 'Search', 'url' => Url::to(['/location/city/index'])],
                         '<li class="divider"></li>',
                         '<li class="dropdown-header">District</li>',
-                        ['label' => 'Create', 'url' => Url::to(['/location/district/create'])],
                         ['label' => 'Search', 'url' => Url::to(['/location/district/index'])],
                         '<li class="divider"></li>',
                         '<li class="dropdown-header">Neighborhood</li>',
-                        ['label' => 'Create', 'url' => '#'],
                         ['label' => 'Search', 'url' => '#'],
                     ]],
                     ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
