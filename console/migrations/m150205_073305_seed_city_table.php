4d9b1cdcea8e648e506a55771f6409dd024e11bc
Vitor Silva <vitor@aleoy.com>
2015-02-05 10:08:36 +0200

    city seed

---
+++ console/migrations/m150205_073305_seed_city_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+use console\migrations\MigrationHelper;
+
+class m150205_073305_seed_city_table extends Migration
+{
+    private $_table = '{{%city}}';
+    private $_catalog = 'location/city';
+
+    public function up()
+    {
+      $columns = MigrationHelper::getColumns($this->_catalog);
+      foreach ($columns as $name) {
+        $this->insert($this->_table, ['name' => $name]);
+      }
+    }
+
+    public function down()
+    {
+      $columns = MigrationHelper::getColumns($this->_catalog);
+      foreach ($columns as $name) {
+        $condition = "name = :name";
+        $params = [':name' => $name];
+        $this->delete($this->_table, $condition, $params);
+      }
+    }
+}
