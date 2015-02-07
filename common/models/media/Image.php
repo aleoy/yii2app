ef4dd08f83b8fce178d025770c177003d5ed8cc5
Vitor Silva <vitor@aleoy.com>
2015-02-07 09:14:11 +0200

    image file manipulation

--- common/models/media/Image.php
+++ common/models/media/Image.php
@@ -19,6 +19,20 @@ use Yii;
  */
 class Image extends \yii\db\ActiveRecord
 {
+    public $allowedExtensions;
+    public $minSize;
+    public $maxSize;
+    public $basedir;
+
+    public function init( )
+    {
+        parent::init();
+        $this->allowedExtensions = ['jpg', 'png', 'gif'];
+        $this->minSize = 20000; // bytes      - 20 kb
+        $this->maxSize = 2147483647; // bytes - 2147.48 mb
+        $this->basedir = \Yii::getAlias('@frontend/web/media/img');
+    }
+
     /**
      * @inheritdoc
      */
@@ -35,6 +49,8 @@ class Image extends \yii\db\ActiveRecord
         return [
             [['path', 'filename', 'extension'], 'required'],
             [['size', 'height', 'width'], 'integer'],
+            ['size', 'compare', 'compareValue' => $this->minSize, 'operator' => '>='],
+            ['size', 'compare', 'compareValue' => $this->maxSize, 'operator' => '<='],
             [['path', 'filename', 'extension'], 'string', 'max' => 255]
         ];
     }
@@ -61,5 +77,83 @@ class Image extends \yii\db\ActiveRecord
     public function getPropertyImages()
     {
         return $this->hasMany(PropertyImage::className(), ['imageId' => 'id']);
+    }
+
+    public function download($uri, $basedir = null)
+    {
+        if(null === $basedir){
+            $basedir = $this->basedir;
+        }
+        $dirname = $this->dirname;
+        $basename = $this->getBasename($uri);
+        $file = $basedir . '/' . $dirname . '/' . $basename;
+
+        if(!is_dir($basedir . '/' . $dirname))
+            mkdir($basedir . '/' . $dirname, 0664, true); //rw-
+
+        file_put_contents($file, fopen($uri, 'r'));
+
+        return $file;
+    }
+
+    public function getdirname()
+    {
+        $dir = date('Y/m/d/H/m');
+
+        return $dir;
+    }
+
+    public function getBasename($uri)
+    {
+        $extension = $this->extensionInfo($uri);
+        $basename = md5($uri) . '.' .$extension;
+
+        return $basename;
+    }
+
+    public function extensionInfo($file)
+    {
+        $path_parts = pathinfo($file);
+        $extension = strtolower($path_parts['extension']);
+
+        return $extension;
+    }
+
+    public function isAllowedExtension($extension)
+    {
+        if(in_array(strtolower($extension), $this->allowedExtensions)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function setFile($file)
+    {
+        if(!is_file($file)){
+            return false;
+        }
+
+        list($path, $basename, $extension, $filename) = array_values( pathinfo($file) );
+        $this->path = $path;
+        $this->extension = $extension;
+        $this->filename = $filename;
+        $this->size = filesize($file);
+        list($this->width, $this->height) = getimagesize($file);
+
+        return true;
+    }
+
+    public function getfile()
+    {
+        return $this->basedir . '/' . $this->path . '/' . $this->filename . '.' . $this->extension;
+    }
+
+    public function afterDelete()
+    {
+        if(is_file($this->file))
+            unlink($this->file);
+
+        return parent::afterDelete();
     }
 }
--- tests/codeception/backend/unit/media/ImageTest.php
+++ tests/codeception/backend/unit/media/ImageTest.php
@@ -32,14 +32,14 @@ class ImageTest extends DbTestCase
         });
 
         //2147483647 bytes - 2147.48 mb
-        $this->specify("path is required", function() {
+        $this->specify("validate max size", function() {
             $model = new Image;
             $model->size = 2147483648;
             $this->assertFalse($model->validate(['size']));
         });
 
         //20000 bytes      - 20 kb
-        $this->specify("path is required", function() {
+        $this->specify("validate min size", function() {
             $model = new Image;
             $model->size = 19999;
             $this->assertFalse($model->validate(['size']));
@@ -78,11 +78,12 @@ class ImageTest extends DbTestCase
     {
         $this->specify("update object from db", function() {
             $model = $this->images('img1');
+            $minSize = $model->minSize;
             $model->attributes = [
                 'path' => '2015/02/05/1',
                 'filename' => 'winter-house',
                 'extension' => 'jpg',
-                'size' => '600',
+                'size' => $minSize,
                 'height' => '100',
                 'width' => '200',
             ];
@@ -102,6 +103,134 @@ class ImageTest extends DbTestCase
 
             $deletedObject = Image::findOne($modelId);
             $this->assertNull($deletedObject);
+        });
+    }
+
+    public function testInit()
+    {
+        $this->specify("test initialization", function() {
+            $model = new Image;
+            
+            $allowedExtensions = ['jpg', 'png', 'gif'];
+            $this->assertSame($model->allowedExtensions, $allowedExtensions);
+
+            $minSize = 20000; // bytes      - 20 kb
+            $this->assertSame($model->minSize, $minSize);
+
+            $maxSize = 2147483647; // bytes - 2147.48 mb
+            $this->assertSame($model->maxSize, $maxSize);
+        });
+    }
+
+    public function testDirname()
+    {
+        $this->specify("test getdirname", function() {
+            $model = new Image;
+            
+            //test is time bound therefore it should not be run close to the time limit 59s
+            //dirname result depends on time
+            while(date('s') > 50){
+                sleep(1);
+            }
+
+            $dirname = date('Y/m/d/H/m');
+            $this->assertSame($model->dirname, $dirname);
+        });
+    }
+
+    public function testExtensionInfo()
+    {
+        $this->specify("test getextensionInfo", function() {
+            $model = new Image;
+            $uri = 'https://i.ss.lv/images/2015-02-06/358695/VHgBGkFmRVk=/1-1.th2.jpg';
+            $this->assertSame($model->extensionInfo($uri), 'jpg');
+        });
+    }
+
+    public function testGetBasename()
+    {
+        $this->specify("test basename", function() {
+            $model = new Image;
+            $uri = 'https://i.ss.lv/images/2015-02-06/358695/VHgBGkFmRVk=/1-1.th2.jpg';
+            $extension = $model->extensionInfo($uri);
+            $basename = md5($uri) . '.' . $extension;
+            $this->assertSame($model->getBasename($uri), $basename);
+        });
+    }
+
+    public function testIsAllowedExtension()
+    {
+        $this->specify("validate allowed extension", function() {
+            $model = new Image;
+            $this->assertTrue($model->isAllowedExtension('jpg'));
+            $this->assertTrue($model->isAllowedExtension('Png'));
+            $this->assertTrue($model->isAllowedExtension('GIF'));
+        });
+
+        $this->specify("validate NOT allowed extension", function() {
+            $model = new Image;
+            $this->assertFalse($model->isAllowedExtension('ppt'));
+            $this->assertFalse($model->isAllowedExtension('DOCX'));
+        });
+    }
+
+    public function testDownload()
+    {
+        $this->specify("test download", function() {
+            $model = new Image;
+            $uri = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/PHP_Logo.png/640px-PHP_Logo.png';
+
+            //test is time bound therefore it should not be run close to the time limit 59s
+            //dirname result depends on time
+            while(date('s') > 50){
+                sleep(1);
+            }
+
+            $dirname = date('Y/m/d/H/m');
+
+            //saving to custom folder
+            $basedir = \Yii::getAlias('@tests/codeception/backend/_output');
+            $file = $basedir . '/'. $model->dirname . '/' . $model->getBasename($uri);
+
+            $this->assertEquals($model->download($uri, $basedir), $file);
+            $this->assertTrue(is_file($file));
+            unlink($file);
+
+            //saving to default folder
+            $basedir = \Yii::getAlias('@frontend/web/media/img');
+            $file = $basedir . '/'. $model->dirname . '/' . $model->getBasename($uri);
+
+            $this->assertEquals($model->download($uri), $file);
+            $this->assertTrue(is_file($file));
+            unlink($file);
+        });
+    }
+
+    public function testSetFile()
+    {
+        $this->specify("test set file", function() {
+            $model = new Image;
+            $basedir = \Yii::getAlias('@tests/codeception/common/fixtures/media/data');
+            $basename = "3760fa29a9ffec6c6590c06f7b4c7d97";
+            $extension = "png";
+            $file = $basedir . '/' . $basename . '.' . $extension;
+
+            $model->setFile($file);
+            $this->assertEquals($model->filename, $basename);
+            $this->assertEquals($model->extension, 'png');
+            $this->assertEquals($model->size, '28756');
+            $this->assertEquals($model->height, '310');
+            $this->assertEquals($model->width, '640');
+        });
+    }
+
+    public function testGetFile()
+    {
+        $this->specify("test get file", function() {
+            $model = $this->images('img1');
+            $file = $model->basedir.'/2015/02/05/1/summer-house.jpg';
+
+            $this->assertEquals($model->file, $file);
         });
     }
 
