57261dffc79e1b383ef197ba7fd4cfe22dd7e80f
Vitor Silva <vitor@aleoy.com>
2015-02-07 19:23:51 +0200

    frontend realestate module

---
+++ frontend/modules/realestate/RealEstate.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\modules\realestate;
+
+class RealEstate extends \yii\base\Module
+{
+    public $controllerNamespace = 'frontend\modules\realestate\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
